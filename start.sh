4549175350032ef8027fac5ee30ecbc6eff5b123
luckybabyboy123 <90592285+luckybabyboy123@users.noreply.github.com>
2021-10-23 13:14:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=d2303068-1a0c-11ec-9621-0242ac130002
+AUUID=c0b3ef90-3402-11ec-8d3d-0242ac130003
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
